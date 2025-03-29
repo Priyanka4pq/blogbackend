0e53b6700c675b55538a11ae5cccf294d727b217
Priyanka Prajapati <priyankanp@acem.edu.in>
2025-03-29 17:24:27 +0530

    second commit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use("/api/auth", userRoutes);
 app.use("/api/blogs", blogRoutes);
 app.use("/api/comments", commentRoutes);
 app.get("/", (req, res) => {
-  res.send("Everything works now!");
+  res.send("Hello There!");
 });
 async function main() {
   await mongoose.connect(process.env.MONGODB_URL);
